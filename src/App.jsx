cda72fd66af1171fe9ee4e546dbd2dd5377a8aaf
Marco Fiordi <marco.fiordi97@gmail.com>
2026-05-04 15:57:22 +0200

    assegnata alla variabile articles un array di oggetti con id titolo e contenuto, aggiunta una map per iterare l'intero array di oggetti per creare un contenuto statico

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 
 
+
 function App() {
-  return (<>
-  <h1>react-form</h1>
-  </>
-    
+  const articles = [
+    {
+      id: 1,
+      titolo: "articolo 1",
+      contenuto: "contenuto di articolo 1,"
+    },
+    {
+      id: 2,
+      titolo: "articolo 2",
+      contenuto: "contenuto di articolo 2,"
+    },
+    {
+      id: 3,
+      titolo: "articolo 3",
+      contenuto: "contenuto di articolo 3,"
+    },
+
+  ]
+  return (
+    <>
+      <h1>react-form</h1>
+      <section>
+        {articles.map(function(article){
+          return (
+            <article key={article.id}>
+              <h2>{article.titolo}</h2>
+              <p>{article.contenuto}</p>
+            </article>
+          );
+        })}
+      </section>
+    </>
+
   );
 }
 export default App;
